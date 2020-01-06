c67ab590ad29fca4948178bb070a606dce4b9031
Brandon Lucia <blucia@gmail.com>
2020-01-06 17:45:10 -0500

    another another rewrite rewrite.  confusing when to box and not.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
 //use std::cmp::Ordering;
 
-//extern crate rand;
-//use rand::Rng;
+extern crate rand;
+use rand::Rng;
+
 
 /*Vertex requires printability and ordering*/
 trait VtxTrait: Ord + std::fmt::Debug + std::fmt::Display {}
 impl<T> VtxTrait for T where T: Ord + std::fmt::Debug + std::fmt::Display {}
 
 #[derive(Debug)]
-enum Graph<'a, T: VtxTrait> {
+enum Graph<T: VtxTrait> {
   G{
-    vtxs: Vec< Box< &'a Vertex<T> > >,
+    vtxs: Vec< Box< Vertex<T> > >,
     next_vtx: usize,
   },
-  Empty
 }
 
 #[derive(Debug)]
@@ -23,26 +23,41 @@ enum Vertex<T: VtxTrait> {
     val: T,
     neigh: Vec<Box<u64>>,
   },
-  Empty
+  Empty,
 }
 
-impl<'a, T: VtxTrait> Graph<'a, T> {
-  fn new() -> Graph<'a, T>{
+impl<T: VtxTrait> Graph< T> {
+  fn new() -> Graph<T>{
     Graph::G{ vtxs: vec![], next_vtx: 0}
   } 
 
-  fn create_vtx(&mut self, v: T) -> Vertex<T>{
+  fn init_vtx(&mut self, ind: u64, v: T){
     match self{
-      Graph::G{ ref mut next_vtx, .. } => { 
-        let next_id = *next_vtx;
-        *next_vtx = *next_vtx + 1;
-        Vertex::new(next_id as u64, v)
-      },
-      Graph::Empty => { Vertex::new(0,v) }
+      Graph::G{ ref mut vtxs, ..} => {
+        vtxs[ind as usize].init(ind,v);
+      }
     }
   }
 
-  fn add_vtx(&mut self, v: &'a Vertex<T>){
+  fn create_vtx(&mut self){
+
+    match self{
+
+      Graph::G{ ref mut next_vtx, ref mut vtxs, .. } => { 
+
+        let next_id = *next_vtx;
+
+        *next_vtx = *next_vtx + 1;
+
+        vtxs.insert(next_id, Box::new(Vertex::Empty) );
+
+      },
+
+    }
+
+  }
+
+  fn add_vtx(&mut self, v: Vertex<T>){
 
     let ins_id: usize;
     match v{
@@ -52,14 +67,12 @@ impl<'a, T: VtxTrait> Graph<'a, T> {
 
     match self{
       Graph::G{ ref mut vtxs, .. } => { vtxs.insert(ins_id, Box::new(v)); },
-      Graph::Empty => return,
     };
   }
 
   fn print(&self){
     match self{
     Graph::G{ ref vtxs, .. } => { for i in 0..vtxs.len() { vtxs[i].print(); } },
-    _ => return
     }
   }
 }
@@ -68,27 +81,19 @@ impl<T: VtxTrait> Vertex<T> {
 
   fn new(id: u64, nv: T) -> Vertex<T> {
 
-    Vertex::V{ id: id, val: nv, neigh: vec![] }
+    Vertex::Empty
 
   }
 
-  //fn add_neigh(&mut self, id: u64) -> Result{//TODO
-  fn add_id(&mut self, id: u64){
-
+  fn init(&mut self, new_id: u64, nv: T){
     match self{
-
-      &mut Vertex::V{ ref mut neigh, ..}  => {
-        neigh.push( Box::new(id) );
-      },
-
+      &mut Vertex::V{ .. } => {},
       &mut Vertex::Empty => {
-        return;
+        *self = Vertex::V{ id: new_id, val: nv, neigh: vec![] };
       }
-
     }
-
   }
-  
+
   fn add_vtx(&mut self, nv: &Vertex<T>){
 
     match self{
@@ -98,13 +103,12 @@ impl<T: VtxTrait> Vertex<T> {
         let new_id: u64; 
         match nv{
           Vertex::V{ ref id, .. } => new_id = *id,
-          Vertex::Empty => { return; }
+          Vertex::Empty => return,
         }
         neigh.push(Box::new(new_id))
 
       },
-      &mut Vertex::Empty => { return; }
-
+      Vertex::Empty => return,
     }
 
   }
@@ -130,8 +134,7 @@ impl<T: VtxTrait> Vertex<T> {
         println!("|----|");
 
       },
-
-      _ => return
+      Vertex::Empty => return,
 
     }
 
@@ -150,28 +153,14 @@ fn prt<T: VtxTrait>(v: &T){
 
 fn main(){
 
-  let mut g: Graph<u64> = Graph::new();
-  let mut t = g.create_vtx(9264);
-  let mut u = g.create_vtx(8111);
-  let mut v = g.create_vtx(7777);
-  let mut z = g.create_vtx(6666);
+  let mut rng = rand::thread_rng();
 
-  t.add_vtx(&u);
-  t.add_vtx(&v);
-  g.add_vtx(&t);
-
-  u.add_vtx(&v);
-  u.add_vtx(&z);
-  g.add_vtx(&u);
-
-  v.add_vtx(&z);
-  v.add_vtx(&t);
-  g.add_vtx(&v);
-
-  z.add_vtx(&t);
-  z.add_vtx(&u);
-  g.add_vtx(&z);
-
-  g.print();
+  let mut gg: Graph<u64> = Graph::new();
+  for i in 0..1000 {
+    gg.create_vtx( );
+    gg.init_vtx( i, rng.gen_range(0,2000000) as u64 );
+    gg.add_neigh( i, 
+  }
+  gg.print();
 
 }
