2f679abe58dd69d78890525d266e3c552df1602f
Brandon Lucia <blucia@gmail.com>
2020-12-28 22:52:43 -0500

    added example of using scan, bfs traversal, and accumulating state during bfs traversal

---
+++ examples/csr_bfs.rs
@@ -0,0 +1,61 @@
+/*
+Copyright 2020 Brandon Lucia <blucia@gmail.com>
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+extern crate rand;
+use rand::Rng;
+
+extern crate raphy;
+use raphy::csr::CSR;
+
+fn main(){
+
+  let mut rng = rand::thread_rng();
+  let mut el = Vec::new();
+ 
+  const NUMV: usize = 10;
+
+  const MAX_E: usize = 4;
+
+  const MAX_WEIGHT: usize = 1000;
+
+  for i in 0..NUMV { 
+
+    /*edges per vertex*/
+    let num_e: usize = rng.gen_range(0,MAX_E) as usize;
+    for _ in 0..num_e{
+  
+      let edge = (i as usize, rng.gen_range(0,NUMV) as usize, rng.gen_range(0,MAX_WEIGHT) as u64);
+
+      el.push(edge);
+
+    }
+
+  }
+  
+  let csr = CSR::new(NUMV,el);
+
+  println!("Edge List");
+  let mut edge_cnt = 0;
+  csr.read_only_scan(|v0,v1,w| {edge_cnt = edge_cnt + 1; println!("{}--{}-->{})",v0,w,v1)});
+  println!("Saw {} Edges",edge_cnt);
+ 
+  println!("BFS Traversal");
+  let bfs_vtxs = &mut Vec::new();
+  let start_v: usize = rng.gen_range(0,NUMV) as usize;
+  csr.bfs_traversal(start_v, |v| { bfs_vtxs.push(v); });
+
+  for v in bfs_vtxs {
+    println!("{}",v);
+  }
+
+}
