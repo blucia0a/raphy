0403e70d161c9c34c69806a6adcc0d6bdfde00cb
Brandon Lucia <blucia@gmail.com>
2022-07-24 21:25:02 -0400

    added smaller large graph

--- examples/csr_mmap.rs
+++ examples/csr_mmap.rs
@@ -4,6 +4,6 @@ extern crate raphy;
 use raphy::csr::CSR;
 
 fn main() {
-    let csr = CSR::new_from_el_mmap(1000000,String::from("graphs/large.el"));
+    let csr = CSR::new_from_el_mmap(500000,String::from("graphs/large.el"));
     csr.write_fastcsr(String::from("graphs/large.csr"));
 }
